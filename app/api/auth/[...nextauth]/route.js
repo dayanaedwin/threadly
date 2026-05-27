0bc25af85f6a38754c4705baa7ff8a975c13dcbf
dayanaedwin <dayanaedwin2016@gmail.com>
2026-05-27 23:53:04 +0530

    feat: initialize Prisma setup with SQLite database and create initial schema
    - Added prisma.config.ts for Prisma configuration.
    - Created migration SQL file for initial database schema including tables for Account, Session, User, VerificationToken, Topic, Post, and Comment.
    - Added migration lock file for version control.
    - Defined Prisma schema in schema.prisma with models and relationships.

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,9 @@
+export { GET, POST } from '@auth';
+
+/*
+    The name [...nextauth] is just a convention, 
+    The important part is the [...] — that's Next.js syntax for a catch-all route. 
+    It means "match any URL segment after /api/auth/".
+    So [...nextauth] = catch-all route, named nextauth just to make it obvious what it's for.
+*/
+
